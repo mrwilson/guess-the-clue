21896a26a0acb44ca2ec2f3ea56a774766822603
Alex Wilson <gpg@probablyfine.co.uk>
2026-01-22 19:12:02 +0000

    refactor: Apply prettier

--- miniclue/index.js
+++ miniclue/index.js
@@ -1,58 +1,64 @@
 export class MiniClue {
-    constructor(answerElement, clueElement) {
-        this.answerElement = answerElement;
-        this.clueElement = clueElement;
-        this.letters = [];
+  constructor(answerElement, clueElement) {
+    this.answerElement = answerElement;
+    this.clueElement = clueElement;
+    this.letters = [];
+  }
+
+  validate(event) {
+    this.answerElement.classList.toggle("validating");
+    if (this.answerElement.checkValidity()) {
+      event.target.remove();
     }
+  }
 
-    validate(event) {
-        this.answerElement.classList.toggle('validating')
-        if (this.answerElement.checkValidity()) {
-            event.target.remove();
-        }
+  renderClue(params) {
+    let words = params.a.split(" ");
+
+    this.clueElement.textContent = `${params.c} (${words.map((x) => x.length).join(",")})`;
+
+    words
+      .map((x) => this.#createWord(x))
+      .forEach((l) => this.answerElement.appendChild(l));
+
+    this.#navigate();
+  }
+
+  #navigate() {
+    for (let i = 0; i < this.letters.length - 1; i++) {
+      const [win1, win2] = this.letters.slice(i, i + 2);
+
+      win1.oninput = (_) => {
+        win1.value && win2.focus();
+      };
+      win2.onkeydown = (e) => {
+        !win2.value && e.keyCode === 8 && win1.focus();
+      };
     }
+  }
 
-    renderClue(params) {
-        let words = params.a.split(' ')
+  #createWord(word) {
+    let group = document.createElement("div");
+    group.classList.add("word");
 
-        this.clueElement.textContent = `${params.c} (${words.map(x => x.length).join(",")})`;
+    word
+      .split("")
+      .map((letter) => this.#createLetter(letter))
+      .forEach((l) => group.appendChild(l));
 
-        words.map(x => this.#createWord(x))
-            .forEach(l => this.answerElement.appendChild(l));
+    return group;
+  }
 
-        this.#navigate()
-    }
+  #createLetter(letter) {
+    let l = document.createElement("input");
+    l.classList.add("letter");
+    l.pattern = `[${letter.toUpperCase()}${letter.toLowerCase()}]`;
+    l.required = true;
+    l.maxLength = 1;
+    l.minLength = 1;
+    l.setAttribute("aria-label", "letter");
+    this.letters.push(l);
+    return l;
 
-    #navigate() {
-        for (let i = 0; i < this.letters.length - 1; i++) {
-            const [win1, win2] = this.letters.slice(i, i+2);
-
-            win1.oninput = (_) => { win1.value && win2.focus() };
-            win2.onkeydown = (e) => { !win2.value && e.keyCode === 8 && win1.focus() }
-        }
-    }
-
-    #createWord(word, idx) {
-        let group = document.createElement('div');
-        group.classList.add("word");
-
-        word.split('')
-            .map(letter => this.#createLetter(letter))
-            .forEach(l => group.appendChild(l))
-
-        return group;
-    }
-
-    #createLetter(letter) {
-        let l = document.createElement('input');
-        l.classList.add("letter");
-        l.pattern = `[${letter.toUpperCase()}${letter.toLowerCase()}]`;
-        l.required = true;
-        l.maxLength = 1;
-        l.minLength = 1;
-        l.setAttribute("aria-label", "letter");
-        this.letters.push(l)
-        return l;
-    }
-
+  }
 }
--- test/miniclue.test.js
+++ test/miniclue.test.js
@@ -1,27 +1,26 @@
-import { assert, describe, it } from 'vitest'
-import { MiniClue } from '../miniclue'
+import { assert, describe, it } from "vitest";
+import { MiniClue } from "../miniclue";
 
 // @vitest-environment jsdom
 
-describe('MiniClue', () => {
-    const answer = document.createElement('form'),
-        validate = document.createElement('button'),
-        clue = document.createElement('h1'),
-        miniClue =  new MiniClue(answer, clue);
+describe("MiniClue", () => {
+  const answer = document.createElement("form"),
+    validate = document.createElement("button"),
+    clue = document.createElement("h1");
+  
+  const miniClue = new MiniClue(answer, clue);
 
-    it('can add validation class when required', () => {
-        assert(!answer.classList.contains("validating"));
+  it("can add validation class when required", () => {
+    assert(!answer.classList.contains("validating"));
 
-        miniClue.validate({target: validate})
+    miniClue.validate({ target: validate });
 
-        assert(answer.classList.contains("validating"));
-    });
+    assert(answer.classList.contains("validating"));
+  });
 
-    it('can render a clue', () => {
-        miniClue.renderClue({"c":"What time is it?", "a":"aaaaaa"});
+  it("can render a clue", () => {
+    miniClue.renderClue({ c: "What time is it?", a: "aaaaaa" });
 
-        assert.equal(clue.textContent, "What time is it? (6)");
-    });
-
-})
-
+    assert.equal(clue.textContent, "What time is it? (6)");
+  });
+});
