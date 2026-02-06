59047100ce0cb3aca902568f2c5ed2269dc40dfc
Alex Wilson <gpg@probablyfine.co.uk>
2026-02-06 11:24:24 +0000

    chore: Apply prettier to everything

--- miniclue/clue.js
+++ miniclue/clue.js
@@ -53,7 +53,7 @@ export class CryptickClue {
         this.showHint.onclick = (_) => {
             this.hint.classList.add('hint--revealed');
             this.showHint.disabled = true;
-        }
+        };
     }
 
     shareMessage(location, clipboard) {
